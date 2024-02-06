8935054a78cf4e74bf0e94981149a010adb38d23
goolsbyash <goolsbyashley015@gmail.com>
2024-02-06 10:04:41 -0500

    Added functionality to delete button to use dispatch action.

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,12 +1,22 @@
-export default function TodoItem({todo}) {
+import { useState } from "react";
+
+export default function TodoItem({todo, dispatch}) {
+const [allowEdit, setAllowEdit] = useState(false);
+const [deleteTodo, setDeleteTodo] = useState(todo.title);
+
+const handleDelete = (e) => {
+    e.preventDefault();
+    dispatch({ type: e.target.value, payload: { deleteTodo } });
+}
+
   return (
     <ul>
       <li>
         <p>{todo.title}</p>
-        <b>Mark Completed:</b>
-        <input type="checkbox" />
+        <b> Completed:</b>
+        <input type="checkbox" value="TOGGLE_COMPLETE" defaultChecked={todo.completed}/>
         <br/>
-        <button type="submit">Delete</button>
+        <button type="submit" value="DELETE" onClick={handleDelete}>Delete</button>
       </li>
     </ul>
   );
