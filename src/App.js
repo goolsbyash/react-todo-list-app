ca7fe5b1947b15c37e19b55c7d4c4fb5cc46e59d
goolsbyash <goolsbyashley015@gmail.com>
2024-02-06 09:39:34 -0500

    Moved dispatch logic into TodoList component.

--- src/App.js
+++ src/App.js
@@ -6,23 +6,10 @@ import { todoReducer } from "./reducers/todoReducer.js";
 
 function App() {
   const [todoList, dispatch] = useReducer(todoReducer, initialState);
-  const [todoItem, setTodoItem] = useState("");
-
-  const handleAdd = (e) => {
-    e.preventDefault();
-    dispatch({type: e.target.value, payload: {todoItem}});
-  };
-
+  
   return (
     <div className="App">
       <h1>Todo List App</h1>
-        <input
-          type="text"
-          value={todoItem}
-          onChange={(e) => setTodoItem(e.target.value)}
-          placeholder="Thing to do"
-        />
-        <button type="submit" value="ADD" onClick={handleAdd}>Add</button>
       <TodoList todoList={todoList} dispatch={dispatch}/>
     </div>
   );
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,10 +1,33 @@
 import TodoItem from "./TodoItem";
-export default function TodoList({todoList}) {
-    return(
-        <div>
-        <h2>Todo List</h2>
-        {todoList.map((todo) => <TodoItem todo={todo}/>)}
-        </div>
-        
-    );
+import { useState } from "react";
+
+export default function TodoList({ todoList, dispatch }) {
+  const [title, setTitle] = useState("");
+
+  const handleAdd = (e) => {
+    e.preventDefault();
+    dispatch({ type: e.target.value, payload: { title } });
+    setTitle(""); // reset state of form
+  };
+
+  return (
+    <div>
+      <h2>Add More Tasks</h2>
+      <input
+        type="text"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        placeholder="Thing to do"
+      />
+      <button type="submit" value="ADD" onClick={handleAdd}>
+        Add
+      </button>
+      <hr />
+      <h2>Todo List</h2>
+
+      {todoList.map((todo) => (
+        <TodoItem todo={todo} key={todo.id} dispatch={dispatch} />
+      ))}
+    </div>
+  );
 }
